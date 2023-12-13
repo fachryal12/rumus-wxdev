650006942e78263ba6de2846aae353712d9865e5
fachryal12 <151593733+fachryal12@users.noreply.github.com>
2023-12-13 13:57:07 +0700

    Add files via upload

---
+++ lat4.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A,T,L;
+    
+    cout <<"masukan alas = ";  
+    cin >>A;
+    cout <<"masukan Tinggi = ";
+    cin >>T;
+    L=(A*T)/2;
+    cout <<"luas = ";
+    cout << L << endl;
+    system ("PAUSE");
+    return 0;
+}
+    
